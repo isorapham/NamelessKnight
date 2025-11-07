cf62c9b4fedb26899f9fb6ddf4eed689324e2300
DESKTOP-B0HBIAI\Admin <isorapnad@gmail.com>
2025-11-07 20:57:14 +0700

    Add Human Assets 6

---
+++ Assets/Character/InfinityPBR - Magic Pig Games/Shared Files/Scripts/LayerField.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace MagicPigGames
+{
+    /// <summary>
+    /// Property attribute to display a layer field in the inspector.
+    /// </summary>
+    public class LayerField : PropertyAttribute {}
+}
